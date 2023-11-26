13600d31b4a3120b35602fc75dc3611dac818c02
mab0y <mab0y@outlook.com>
2023-11-25 19:34:53 -0500

    rpg revised

--- js/games.js
+++ js/games.js
@@ -1,17 +1,27 @@
-document.querySelectorAll('.flip-card').forEach(card => {
-    card.addEventListener('click', function() {
-        const front = this.querySelector('.flip-card-front');
-        const back = this.querySelector('.flip-card-back');
-        if (front.style.zIndex === "2") {
-            front.style.zIndex = "1";
-            back.style.zIndex = "2";
-            front.style.transform = "translate(3%, 3%)"; 
-            back.style.transform = "translate(-3%, -3%)"; 
-        } else {
-            front.style.zIndex = "2";
-            back.style.zIndex = "1";
-            front.style.transform = "translate(-3%, -3%)"; 
-            back.style.transform = "translate(3%, 3%)";
-        }
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('.flip-card').forEach(card => {
+        const front = card.querySelector('.flip-card-front');
+        const back = card.querySelector('.flip-card-back');
+
+
+        front.style.zIndex = "2";
+        back.style.zIndex = "1";
+        front.style.transform = "translate(-3%, -3%)";
+        back.style.transform = "translate(3%, 3%)";
+
+
+        card.addEventListener('click', function() {
+            if (front.style.zIndex === "2") {
+                front.style.zIndex = "1";
+                back.style.zIndex = "2";
+                front.style.transform = "translate(3%, 3%)"; 
+                back.style.transform = "translate(-3%, -3%)"; 
+            } else {
+                front.style.zIndex = "2";
+                back.style.zIndex = "1";
+                front.style.transform = "translate(-3%, -3%)"; 
+                back.style.transform = "translate(3%, 3%)";
+            }
+        });
     });
 });
