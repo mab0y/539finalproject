28cd6b557b2cab4ec4d78193d53f145f3602981f
mab0y <mab0y@outlook.com>
2023-11-26 19:10:04 -0500

    bug fixed

--- js/games.js
+++ js/games.js
@@ -3,25 +3,34 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const front = card.querySelector('.flip-card-front');
         const back = card.querySelector('.flip-card-back');
 
-
         front.style.zIndex = "2";
         back.style.zIndex = "1";
         front.style.transform = "translate(-3%, -3%)";
         back.style.transform = "translate(3%, 3%)";
-
 
         card.addEventListener('click', function() {
             if (front.style.zIndex === "2") {
                 front.style.zIndex = "1";
                 back.style.zIndex = "2";
                 front.style.transform = "translate(3%, 3%)"; 
-                back.style.transform = "translate(-3%, -3%)"; 
+                back.style.transform = "translate(-3%, -3%)";
+                back.classList.remove('floating');
             } else {
                 front.style.zIndex = "2";
                 back.style.zIndex = "1";
                 front.style.transform = "translate(-3%, -3%)"; 
                 back.style.transform = "translate(3%, 3%)";
             }
+        });
+
+        card.addEventListener('mouseenter', function() {
+            if (front.style.zIndex === "2") {
+                back.classList.add('floating');
+            }
+        });
+
+        card.addEventListener('mouseleave', function() {
+            back.classList.remove('floating');
         });
     });
 });
