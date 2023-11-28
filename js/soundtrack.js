4c977dfc1240c63dcd7d9353fb34dee322c94291
mab0y <mab0y@outlook.com>
2023-11-28 16:59:47 -0500

    All Done

--- js/soundtrack.js
+++ js/soundtrack.js
@@ -12,32 +12,44 @@ document.addEventListener('DOMContentLoaded', function() {
 
     image1.addEventListener('click', function() {
         iframe.src = 'https://open.spotify.com/embed/album/6pYNEn4tMc6gdv5fIZf5yn?utm_source=generator';
+        scrollToIframe();
     });
     image2.addEventListener('click', function() {
         iframe.src = 'https://open.spotify.com/embed/album/5Oli3gQJrHdahY7FDEoofW?utm_source=generator';
+        scrollToIframe();
     });
 
     image3.addEventListener('click', function() {
         iframe.src = 'https://open.spotify.com/embed/playlist/2CaO2my68JsZ4CLiL1UVJB?utm_source=generator';
+        scrollToIframe();
     });
 
     image4.addEventListener('click', function() {
         iframe.src = 'https://open.spotify.com/embed/album/0H4cXrYB3b8IDEFgTMNcAT?utm_source=generator';
+        scrollToIframe();
     });
 
     image5.addEventListener('click', function() {
         iframe.src = 'https://open.spotify.com/embed/playlist/0sl9ccXJZol3YmVmnrCJGD?utm_source=generator';
+        scrollToIframe();
     });
 
     image6.addEventListener('click', function() {
         iframe.src = 'https://open.spotify.com/embed/album/09ixa10YNG75H0HDKbMAiZ?utm_source=generator';
+        scrollToIframe();
     });
 
     image7.addEventListener('click', function() {
         iframe.src = 'https://open.spotify.com/embed/album/2YFDpVVSzAtOMll0CyPfms?utm_source=generator';
+        scrollToIframe();
     });
 
     image8.addEventListener('click', function() {
         iframe.src = 'https://open.spotify.com/embed/playlist/37i9dQZF1DX3rXtgePifMs?utm_source=generator';
+        scrollToIframe();
     });
+
+    function scrollToIframe() {
+        iframe.scrollIntoView({behavior: 'smooth'});
+    }
 });
