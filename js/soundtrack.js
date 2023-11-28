fdd95fdb24c6d11b5f58e21c039aa64fcf117f93
mab0y <mab0y@outlook.com>
2023-11-27 22:33:56 -0500

    soundtrack page done

---
+++ js/soundtrack.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    var iframe = document.getElementById('spotify-link');
+    var image1 = document.getElementById('witcher');
+    var image2 = document.getElementById('redemption');
+    var image3 = document.getElementById('bf');
+    var image4 = document.getElementById('mh');
+    var image5 = document.getElementById('civilization');
+    var image6 = document.getElementById('assassin');
+    var image7 = document.getElementById('dota');
+    var image8 = document.getElementById('fc');
+
+    image1.addEventListener('click', function() {
+        iframe.src = 'https://open.spotify.com/embed/album/6pYNEn4tMc6gdv5fIZf5yn?utm_source=generator';
+    });
+    image2.addEventListener('click', function() {
+        iframe.src = 'https://open.spotify.com/embed/album/5Oli3gQJrHdahY7FDEoofW?utm_source=generator';
+    });
+
+    image3.addEventListener('click', function() {
+        iframe.src = 'https://open.spotify.com/embed/playlist/2CaO2my68JsZ4CLiL1UVJB?utm_source=generator';
+    });
+
+    image4.addEventListener('click', function() {
+        iframe.src = 'https://open.spotify.com/embed/album/0H4cXrYB3b8IDEFgTMNcAT?utm_source=generator';
+    });
+
+    image5.addEventListener('click', function() {
+        iframe.src = 'https://open.spotify.com/embed/playlist/0sl9ccXJZol3YmVmnrCJGD?utm_source=generator';
+    });
+
+    image6.addEventListener('click', function() {
+        iframe.src = 'https://open.spotify.com/embed/album/09ixa10YNG75H0HDKbMAiZ?utm_source=generator';
+    });
+
+    image7.addEventListener('click', function() {
+        iframe.src = 'https://open.spotify.com/embed/album/2YFDpVVSzAtOMll0CyPfms?utm_source=generator';
+    });
+
+    image8.addEventListener('click', function() {
+        iframe.src = 'https://open.spotify.com/embed/playlist/37i9dQZF1DX3rXtgePifMs?utm_source=generator';
+    });
+});
